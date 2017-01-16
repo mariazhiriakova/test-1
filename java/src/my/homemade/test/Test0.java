b16a72fa9d23d6d60a193437c510a4bf21cdd66b
mariazhiriakova <maria.zhiriakova@gmail.com>
2017-01-16 16:37:56 +0200

    Update Test0.java

--- java/src/my/homemade/test/Test0.java
+++ java/src/my/homemade/test/Test0.java
@@ -3,6 +3,6 @@ package my.homemade.test;
 public class Test0 {
     public static void main(String[] args) {
         System.out.println("!");
-        System.out.println("AAA");
+        System.out.println("AAA BBB");
     }
 }
